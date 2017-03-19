d5ac527d27d51675c122325b2709a7c3210e040e
Nidhi Baheti <nidhibaheti1991@gmail.com>
2017-03-18 17:12:57 -0700

    Added user auth

--- challenge/src/main/java/com/thousandeyes/SecurityConfiguration.java
+++ challenge/src/main/java/com/thousandeyes/SecurityConfiguration.java
@@ -12,13 +12,28 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-            .antMatchers("/h2-console/*","/greeting","/test","/person","/followers","/following","/follow","/unfollow","/messages").permitAll().anyRequest().authenticated();
-
-
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1").password("secret1").roles("USER")
+                .and()
+                .withUser("user2").password("secret2").roles("USER");
     }
 
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//            .antMatchers("/h2-console/*","/test","/person","/followers","/following","/follow","/unfollow","/messages").permitAll().anyRequest().authenticated();
+//
+//
+//        http.csrf().disable();
+//        http.headers().frameOptions().disable();
+//    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().fullyAuthenticated();
+        http.httpBasic();
+        http.csrf().disable();
+    }
 }
