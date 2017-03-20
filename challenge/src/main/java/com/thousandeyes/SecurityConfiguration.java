c0a4b3a035377d3693799154c7b977982a6627b7
Nidhi Baheti <nidhibaheti1991@gmail.com>
2017-03-19 22:07:38 -0700

    Clean up Security Config file

--- challenge/src/main/java/com/thousandeyes/SecurityConfiguration.java
+++ challenge/src/main/java/com/thousandeyes/SecurityConfiguration.java
@@ -59,35 +59,4 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
     }
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/", "/home").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin()
-//                .loginPage("/login")
-//                .permitAll()
-//                .and()
-//                .logout()
-//                .permitAll();
-//    }
-    
-//        @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//          .authorizeRequests()
-//          .antMatchers("/","/home").permitAll()
-//          .antMatchers("/admin").hasRole("ADMIN")
-//          .anyRequest().authenticated()
-//          .and()
-//          .formLogin()
-//          .loginPage("/login")
-//          .permitAll()
-//          .and()
-//          .logout()
-//          .permitAll();
-//        http.exceptionHandling().accessDeniedPage("/403");
-//    }
 }
